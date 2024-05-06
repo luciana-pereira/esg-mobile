628274361b91e3d7a63540f4332881003e54743f
l.dos.santos.souza <luci.lv14@gmail.com>
2024-05-05 21:41:34 -0300

    update config project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "br.com.fiap.esg"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
